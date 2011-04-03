25b1007886b6eef6e85a9efbcc5ee5dca17bd1a9
Timo Härkönen <timop.harkonen@gmail.com>
2011-04-03 18:58:31 +0300

    incomplete pdf printing

--- src/pdfwriter.cpp
+++ src/pdfwriter.cpp
@@ -1,10 +1,18 @@
 #include "pdfwriter.h"
 
 #include <QDebug>
+#include <QPainter>
+#include <QPrinter>
+#include <QDir>
+#include <QDateTime>
+#include <QImage>
+#include <QSize>
+#include <QFont>
 
 PdfWriter::PdfWriter(QObject *parent) :
-    QObject(parent)
+    QThread(parent)
 {
+    m_image = "";
 }
 
 void PdfWriter::create(const QList<QPair<QString, QString> > &content, const QString &image)
@@ -13,11 +21,77 @@ void PdfWriter::create(const QList<QPair<QString, QString> > &content, const QSt
         qDebug() << "empty list at" << __PRETTY_FUNCTION__;
         return;
     }
-    qDebug() << __PRETTY_FUNCTION__ << "is not implement..";
-    qDebug() << "bgimage" << image;
-    qDebug() << "content:";
 
-    for(int i = 0; i < content.count(); i++) {
-        qDebug() << content.at(i).first << content.at(i).second;
+    m_content = content;
+    m_image = image;
+
+    start();
+}
+
+void PdfWriter::run()
+{
+    printTags();
+}
+
+void PdfWriter::printTags()
+{
+    QPrinter printer;
+    printer.setOutputFormat(QPrinter::PdfFormat);
+    printer.setPaperSize(QPrinter::A4);
+    printer.setResolution(QPrinter::HighResolution);
+
+    //this could be asked from the user..
+    printer.setOutputFileName(QString("%1/nametags-%2.pdf")
+                              .arg(QDir::homePath())
+                              .arg(QDateTime::currentDateTime().toString("yyyyMMddhhmm")));
+
+    QImage image;
+    if(QFile::exists(m_image)) {
+        image.load(m_image);
     }
+    else {
+        image.load(":/pics/summit-fi.jpg");
+    }
+    QPainter painter;
+    painter.begin(&printer);
+    QFont firstNameFont = painter.font();
+    firstNameFont.setPointSize(72);
+
+    QFont lastNameFont = painter.font();
+    lastNameFont.setPointSize(48);
+
+    painter.setPen(Qt::black);
+    for(int i = 0; i < m_content.count(); i++) {
+        // first name
+        //painter.drawImage(0,0, image);
+        painter.setFont(firstNameFont);
+        painter.drawText(QRect(0, 0, 100, 100), m_content.at(i).first);
+
+        // last name
+        painter.setFont(lastNameFont);
+        painter.drawText(QRect(0, 74, 100, 100), m_content.at(i).second);
+
+        // second name tag to the page if needed
+        if(i+1 < m_content.count()) {
+            i++;
+            // first name
+            //painter.setBackground(QBrush(image));
+            //painter.drawImage(0,0, image);
+            painter.setFont(firstNameFont);
+            painter.drawText(QRect(0, 100, 100, 100), m_content.at(i).first);
+
+            // last name
+            painter.setFont(lastNameFont);
+            painter.drawText(QRect(0, 174, 100, 100), m_content.at(i).second);
+        }
+
+        // next page if needed
+        if(i + 1 < m_content.count()) {
+            printer.newPage();
+        }
+    }
+
+    painter.end();
+
+    emit printingFinished();
 }
--- src/pdfwriter.h
+++ src/pdfwriter.h
@@ -1,19 +1,30 @@
 #ifndef PDFWRITER_H
 #define PDFWRITER_H
 
-#include <QObject>
+#include <QThread>
 #include <QPair>
 
-class PdfWriter : public QObject
+class PdfWriter : public QThread
 {
     Q_OBJECT
 public:
     explicit PdfWriter(QObject *parent = 0);
 
 signals:
+    void printingFinished();
+
+protected:
+    void run();
 
 public slots:
     void create(const QList<QPair<QString,QString> > &content, const QString &image);
+
+private:
+    void printTags();
+
+private:
+    QList<QPair<QString,QString> > m_content;
+    QString m_image;
 
 };
 
