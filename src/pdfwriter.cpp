ec004f9303b3bb077715834a1526dc2c5183ec9c
Sampo Saaristo <sampo.saaristo@gmail.com>
2011-04-03 22:30:54 +0300

    trialling with proper place for text

--- src/pdfwriter.cpp
+++ src/pdfwriter.cpp
@@ -57,12 +57,16 @@ void PdfWriter::printTags()
     QPainter painter;
     painter.begin(&printer);
     QFont firstNameFont = painter.font();
-    firstNameFont.setPointSize(72);
+    firstNameFont.setPointSize(40);
 
     QFont lastNameFont = painter.font();
-    lastNameFont.setPointSize(48);
+    lastNameFont.setPointSize(24);
 
     QRectF upperHalf(0, 0, printer.pageRect().width(), printer.pageRect().height() / 2);
+    qreal w = (upperHalf.width()/2);
+    qreal h = (upperHalf.height()/2);
+    QRectF upperFirstNameRect (w, h + h/3, w, h/3);
+    QRectF upperLastNameRect (w, h + h/3, w, 2*h/3);
     QRectF lowerHalf(0, printer.pageRect().height() / 2, printer.pageRect().width(), printer.pageRect().height() / 2);
 
     painter.setPen(Qt::black);
@@ -71,12 +75,12 @@ void PdfWriter::printTags()
         painter.setFont(firstNameFont);
         painter.setBrush(Qt::black);
         painter.drawImage(upperHalf, image, image.rect());
-        //painter.drawText(QRect(0, 0, 100, 100), m_content.at(i).first);
+        painter.drawText(upperFirstNameRect, Qt::AlignHCenter | Qt::AlignBottom, m_content.at(i).first);
 
         // last name
         painter.setFont(lastNameFont);
         painter.drawImage(lowerHalf, image);
-        //painter.drawText(QRect(0, 74, 100, 100), m_content.at(i).second);
+        painter.drawText(upperLastNameRect, Qt::AlignCenter, m_content.at(i).second);
 
         // second name tag to the page if needed
         if(i+1 < m_content.count()) {
